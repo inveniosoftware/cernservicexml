ffc384cd5ce324f5624048ed196937daf344b908
Lars Holm Nielsen <lars.holm.nielsen@cern.ch>
2017-05-12 13:20:19 +0200

    global: remove python 2.6 and 3.3 support
    * Remove Python 2.6 support because pydocstyle is not compatible.
    
    * Remove Python 3.3 support because Sphinx needs at least Python
      2.7/3.4.
    
    Signed-off-by: Lars Holm Nielsen <lars.holm.nielsen@cern.ch>

--- setup.py
+++ setup.py
@@ -56,10 +56,8 @@ setup(
     },
     classifiers=[
         'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
         'Development Status :: 5 - Production/Stable',
         'Intended Audience :: Developers',
